b34e2f679946f8bfe06dc1b7a6c9bdacd37ee572
guiklimek <guillaume.klimek@tutanota.com>
2023-09-16 03:16:29 +0200

    back: delete hello test

--- src/back/src/graphql/resolvers.ts
+++ src/back/src/graphql/resolvers.ts
@@ -10,13 +10,6 @@ export const resolvers = {
         }
     },
     Subscription: {
-        hello: {
-            subscribe: async function* () {
-                for await (const t of ['WAW/E8164/1', 'WAW/E8164/2', 'WAW/E8164/3']) {
-                    yield {topic: t, payload: 'pouet'};
-                }
-            }
-        },
         messageReceived: {
             // The AsyncIterator method will tell the MQTT client to listen for messages from the MQTT broker on the topic provided,
             // and wraps that listener in an AsyncIterator object.
