5daff743005b9b4988a71c45dc955ad1354f0fa5
guiklimek <guillaume.klimek@tutanota.com>
2023-11-23 23:31:34 +0100

    front: I don't create tree this way

--- src/front/src/utils/utils.ts
+++ src/front/src/utils/utils.ts
@@ -1,25 +1,7 @@
-import {ReadyNodeType, NodeType} from "./types";
 
 export const utils: any = {
     // This should be array.includes('value') but it's to early to use (ES7)
     // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
     includes: (value: any, array: Array<any>) => array.indexOf(value) !== -1,
-    createTree: (node: NodeType, nodes: NodeType[], readyNodes: ReadyNodeType[]) => {
-        const newNode: NodeType = {
-            id: node.id,
-            label: node.label,
-            subnodes: []
-        }
-        let item: ReadyNodeType = {id: node.id, isParent: false};
-        if (node.subnodes !== null && node.subnodes.length > 0) {
-            item.isParent = true;
-            node.subnodes.map((subNode: NodeType) => {
-                const eq = nodes.find((node: NodeType) => node.id === subNode.id);
-                if (eq !== undefined) newNode.subnodes.push(utils.createTree(eq, nodes, readyNodes));
-            });
-        }
-        readyNodes.push(item);
-        return newNode;
-    },
     dateFrom: (timestamp: number): string => new Date(timestamp).toISOString()
 }
