5cc0745378dc63b81ec935cb0c2497af01a86483
guiklimek <guillaume.klimek@tutanota.com>
2023-12-05 18:35:49 +0100

    simulator: args + 4 nodes 4 devices by default + end at the end

--- simulator/src/index.ts
+++ simulator/src/index.ts
@@ -4,12 +4,20 @@ import {encodePayload, UPayload} from "sparkplug-payload/lib/sparkplugbpayload";
 import SETTINGS from "../../settings.json";
 import {utils} from "./utils";
 
+let nbNodes: number = 4;
+let nbDevices: number = 4;
+if (process.argv.length > 3) {
+    let nbNodes: number = parseInt(process.argv[2]);
+    let nbDevices: number = parseInt(process.argv[3]);
+}
 
 // Default MQTT broker url
 let brokerUrl: string = `${SETTINGS.mqttServer.mqtt}://${SETTINGS.mqttServer.host}:${SETTINGS.mqttServer.port}`;
 const topicBase: string = 'spBv1.0/GROUPID';
 
+
 (async () => {
+
     // ******************************************
     // * MQTT Client
     // ******************************************
@@ -18,49 +26,53 @@ const topicBase: string = 'spBv1.0/GROUPID';
     // ******************************************
     // * Nodes publish
     // ******************************************
-    let nodeTopic: string = `${topicBase}/${utils.euidGenerator()}`;
+    for await (const iNode of utils.range(nbNodes)) {
 
-    const payload: UPayload = {
-        timestamp: new Date().getTime(),
-        metrics: [{
-            name: 'Node',
-            type: 'String',
-            value: 'PoUeT pOuEt',
-            properties: {Unit: {value: 'pp', type: 'String'}},
-        }]
-    };
-    const encodedPayload: Uint8Array = encodePayload(payload);
-
-    console.log('Publish on', nodeTopic);
-    mqttClient.publish(nodeTopic, Buffer.from(encodedPayload));
-    await utils.sleep(1000);
-
-    // ******************************************
-    // * Devices publish
-    // ******************************************
-    let value: boolean = true;
-
-    for await (const i of utils.range(4)) {
-
-        const deviceTopic: string = `${nodeTopic}/${utils.euidGenerator()}`;
+        let nodeTopic: string = `${topicBase}/${utils.euidGenerator()}`;
 
         const payload: UPayload = {
             timestamp: new Date().getTime(),
             metrics: [{
-                name: `Device${i} Boolean`,
-                type: 'Boolean',
-                value,
-            }, {
-                name: `Device${i} String`,
+                name: `Node${iNode}}`,
                 type: 'String',
-                value: `[${i * i + i}]`,
+                value: 'PoUeT pOuEt',
+                properties: {Unit: {value: 'pp', type: 'String'}},
             }]
         };
-        value = !value;
         const encodedPayload: Uint8Array = encodePayload(payload);
 
-        console.log('Publish on', deviceTopic);
-        mqttClient.publish(deviceTopic, Buffer.from(encodedPayload));
+        console.log('Publish on', nodeTopic);
+        mqttClient.publish(nodeTopic, Buffer.from(encodedPayload));
         await utils.sleep(1000);
+
+        // ******************************************
+        // * Devices publish
+        // ******************************************
+        let value: boolean = true;
+
+        for await (const iDevice of utils.range(nbDevices)) {
+
+            const deviceTopic: string = `${nodeTopic}/${utils.euidGenerator()}`;
+
+            const payload: UPayload = {
+                timestamp: new Date().getTime(),
+                metrics: [{
+                    name: `Device${iDevice} Boolean`,
+                    type: 'Boolean',
+                    value,
+                }, {
+                    name: `Device${iDevice} String`,
+                    type: 'String',
+                    value: `[${iDevice}]`,
+                }]
+            };
+            value = !value;
+            const encodedPayload: Uint8Array = encodePayload(payload);
+
+            console.log('Publish on', deviceTopic);
+            mqttClient.publish(deviceTopic, Buffer.from(encodedPayload));
+            await utils.sleep(1000);
+        }
     }
+    await mqttClient.endAsync();
 })();
