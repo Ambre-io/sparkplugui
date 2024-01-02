21def9f2e1ad0dfe8cde620e0787e13e6d46ea9d
guiklimek <guillaume.klimek@tutanota.com>
2024-01-02 18:47:01 +0100

    first working mqtt client BWOAH

--- sparkplugui/src/components/eventbuttons/ConnectButton.tsx
+++ sparkplugui/src/components/eventbuttons/ConnectButton.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 import CloudOffOutlinedIcon from "@mui/icons-material/CloudOffOutlined";
 import CloudOutlinedIcon from "@mui/icons-material/CloudOutlined";
+import {invoke} from '@tauri-apps/api/tauri';
 import {toast} from "react-toastify";
 import {useDispatch, useSelector} from "react-redux";
-import {useLazyQuery, useMutation} from "@apollo/client";
 import {useTranslation} from "react-i18next";
 
 import {AmbreIconButton} from "../ambre/AmbreIconButton.tsx";
-import {CONNECT, DISCONNECT} from "../../graphql/graphql.ts";
 import {constants} from '../../utils/constants.ts';
 import {getMQTTData} from "../../redux/data/mqttDataSlice.ts";
 import {setReloadEvent} from "../../redux/events/reloadEventSlice.ts";
@@ -23,49 +22,26 @@ export const ConnectButton: React.FC = () => {
 
     const error = () => toast.error(`${t('error')} ${constants.emojiSadge}`);
 
-    const [connect] = useMutation(CONNECT);
-    const [disconnect] = useLazyQuery(DISCONNECT, {fetchPolicy: "no-cache"});
-
     const goClick = async () => {
         if (!connected) {
-            try {
-                const res = await connect({variables: information});
-                const data = res.data.connect;
-                if (data !== null && data) {
-                    dispatch(setReloadEvent()); // reload Messages subscription
-                    toast.success(`${t('successConnect')} ${constants.emojiOkg}`);
-                    setConnected(true);
-                } else {
-                    error();
-                }
-                if (res.errors !== undefined) {
-                    console.debug('ConnectButton: connect error', res.errors);
-                    error();
-                }
-            } catch (e) {
+            invoke('connect'
+            ).then((response) => {
+                dispatch(setReloadEvent()); // reload Messages subscription
+                toast.success(`${t('successConnect')} ${constants.emojiOkg}`);
+                setConnected(true);
+            }).catch(e => {
                 console.debug('ConnectButton: connect fail', e);
                 error();
-            }
+            });
         } else {
-            try {
-                const res = await disconnect();
-                const disconnected = res.data.disconnect;
-                if (disconnected !== null) {
-                    if (disconnected) {
-                        toast.success(`${t('successDisconnect')} ${constants.emojiOkg}`);
-                        setConnected(false);
-                    } else {
-                        error();
-                    }
-                }
-                if (res.error !== undefined) {
-                    console.debug('ConnectButton: disconnect error', res.error);
-                    error();
-                }
-            } catch (e) {
+            invoke('disconnect'
+            ).then((response) => {
+                toast.success(`${t('successDisconnect')} ${constants.emojiOkg}`);
+                setConnected(false);
+            }).catch(e => {
                 console.debug('ConnectButton: disconnect fail', e);
                 error();
-            }
+            });
         }
     };
 
--- sparkplugui/src/main.tsx
+++ sparkplugui/src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {invoke} from '@tauri-apps/api/tauri';
 
 import {App} from './App.tsx';
 
 
-invoke('greet', {name: 'Petit pouet'}
-).then((response) => console.log(response)
-).catch(e => console.error(`Error with the greet invoke: ${e}`));
+// @@@ Tauri invoke example
+// import {invoke} from '@tauri-apps/api/tauri';
+// invoke('greet', {name: 'Petit pouet'}
+// ).then((response) => console.log(response)
+// ).catch(e => console.error(`Error with the greet invoke: ${e}`));
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
