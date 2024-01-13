6de191bc7ba842fd7c073b14f453377c8236f66d
guiklimek <guillaume.klimek@tutanota.com>
2024-01-13 03:15:08 +0100

    front: start React Grid Layout implementation

--- sparkplugui/frontend/src/App.tsx
+++ sparkplugui/frontend/src/App.tsx
@@ -7,8 +7,9 @@ SparkpluGUI - Software that displays decoded Sparkplug messages from MQTT IoT
 * https://ambre.io/
 */
 
-import Grid from "@mui/material/Grid";
 import GridLayout from "react-grid-layout";
+import '/node_modules/react-grid-layout/css/styles.css';
+import '/node_modules/react-resizable/css/styles.css';
 import {Provider} from 'react-redux'
 import 'react-toastify/dist/ReactToastify.css';
 import {ThemeProvider} from "@mui/system";
@@ -29,64 +30,50 @@ import './styles/index.css';
 // RGL performance tips
 // see: https://github.com/react-grid-layout/react-grid-layout#performance
 
-    // (
-    //     <GridLayout
-    //         className="layout"
-    //         layout={layout}
-    //         cols={12}
-    //         rowHeight={30}
-    //         width={1200}
-    //     >
-    //         <div key="a">SoftCard<SoftCard/></div>
-    //         <div key="b">FormCard<FormCard/></div>
-    //         <div key="c">TreeCard<TreeCard/></div>
-    //         <div key="d">LastMessageCard<LastMessageCard/></div>
-    //         <div key="e">MessagesCard<MessagesCard/></div>
-    //     </GridLayout>
-    // );
 
-export const App = () => (
-    <Provider store={store}>
-        <ThemeProvider theme={theme}>
-            <ToastContainer position={toast.POSITION.BOTTOM_RIGHT}/>
-            {/*see: https://alvarotrigo.com/blog/animated-backgrounds-css/#3)-floating-squares*/}
-            <div className="area">
-                <ul className="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <main className="resetCircles">
-                        <Grid container>
-                            <Grid item xs={7}>
-                                <Grid container>
-                                    <Grid item xs={6} sx={styles.padding(2)}>
-                                        <SoftCard/>
-                                    </Grid>
-                                    <Grid item xs={6} sx={styles.padding(2)}>
-                                        <FormCard/>
-                                    </Grid>
-                                    <Grid item xs={6} sx={styles.padding(2)}>
-                                        <TreeCard/>
-                                    </Grid>
-                                    <Grid item xs={6} sx={styles.padding(2)}>
-                                        <LastMessageCard/>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                            <Grid item xs={5} sx={styles.padding(2)}>
-                                <MessagesCard/>
-                            </Grid>
-                        </Grid>
-                    </main>
-                </ul>
-            </div>
-        </ThemeProvider>
-    </Provider>
-);
+export const App = () => {
+    const layout = [
+        {i: "a", x: 0, y: 0, w: 1, h: 2, static: true},
+        {i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+        {i: "c", x: 4, y: 0, w: 1, h: 2},
+        {i: "d", x: 6, y: 0, w: 1, h: 2},
+        {i: "e", x: 0, y: 4, w: 1, h: 2}
+    ];
+    return (
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <ToastContainer position={toast.POSITION.BOTTOM_RIGHT}/>
+                {/*see: https://alvarotrigo.com/blog/animated-backgrounds-css/#3)-floating-squares*/}
+                <div className="area">
+                    <ul className="circles">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <main className="resetCircles">
+                            <GridLayout
+                                className="layout"
+                                layout={layout}
+                                cols={12}
+                                rowHeight={30}
+                                width={1200}
+                            >
+                                <div key="a"><SoftCard/></div>
+                                <div key="b"><FormCard/></div>
+                                <div key="c"><TreeCard/></div>
+                                <div key="d"><LastMessageCard/></div>
+                                <div key="e"><MessagesCard/></div>
+                            </GridLayout>
+                        </main>
+                    </ul>
+                </div>
+            </ThemeProvider>
+        </Provider>
+    );
+};
